a92468f643b4b455903daec2324c3f6975c04f7f
Bernardo Quina <bernardoquina@gmail.com>
2023-05-14 15:59:15 +0100

    Change redirect endpoint logic

--- src/routes/login-redirect/local/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city'
-
-export const onGet: RequestHandler = async (reqEvent) => {
-  throw reqEvent.redirect(302, 'http://127.0.0.1:5173/')
-}
--- src/routes/login-redirect/prod/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city'
-
-export const onGet: RequestHandler = async (reqEvent) => {
-  throw reqEvent.redirect(302, 'https://qwik-drizzle.vercel.app/')
-}
