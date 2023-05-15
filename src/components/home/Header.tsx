dc6299bb94bf16a857614db6a3fb1fe95f92de90
Bernardo Quina <bernardoquina@gmail.com>
2023-05-15 19:35:19 +0100

    apply tailwind class orders

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -2,9 +2,9 @@ import { component$ } from '@builder.io/qwik'
 
 export default component$(() => {
   return (
-    <header class="fixed z-[10] left-0 flex w-full justify-center">
-      <div class="w-[600px] max-w-full border-b-[1px] border-x-[1px] bg-white bg-opacity-50 px-3 backdrop-blur-lg dark:bg-blue-1100 dark:bg-opacity-50">
-        <h1 class="font-semibold text-xl py-3">Home</h1>
+    <header class="fixed left-0 z-[10] flex w-full justify-center">
+      <div class="w-[600px] max-w-full border-x-[1px] border-b-[1px] bg-white bg-opacity-50 px-3 backdrop-blur-lg dark:bg-blue-1100 dark:bg-opacity-50">
+        <h1 class="py-3 text-xl font-semibold">Home</h1>
       </div>
     </header>
   )
