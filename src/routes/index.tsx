f725890f04e217231828ad182d4699d3634f1d0c
Bernardo Quina <bernardoquina@gmail.com>
2023-05-23 14:30:15 +0100

    Infinite scroll on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { component$, useStore } from '@builder.io/qwik'
-import { type DocumentHead, routeLoader$ } from '@builder.io/qwik-city'
+import { component$, useSignal, useStore, useVisibleTask$ } from '@builder.io/qwik'
+import { type DocumentHead, routeLoader$, server$ } from '@builder.io/qwik-city'
 import { desc } from 'drizzle-orm'
 
 import { likes, posts } from '../db/schema'
@@ -15,13 +15,36 @@ export const usePosts = routeLoader$(async (reqEvent) => {
   try {
     const db = getDb(reqEvent)
 
-    const newPosts = await db.query.posts.findMany({
+    const queryPosts = await db.query.posts.findMany({
       with: { author: true },
       extras: { likeCount: countWithColumn(likes.postId.name).as('likeCount') },
+      limit: 25,
       orderBy: desc(posts.createdAt),
     })
 
-    return { code: 200, message: 'success', data: newPosts }
+    return { code: 200, message: 'success', data: queryPosts }
+  } catch (error) {
+    let message = 'Oops, something went wrong. Please try again later.'
+
+    if (error instanceof Error) message = error.message
+
+    return { code: 500, message, data: null }
+  }
+})
+
+export const getPosts = server$(async function ({ offset }: { offset: number }) {
+  try {
+    const db = getDb({ env: this.env })
+
+    const queryPosts = await db.query.posts.findMany({
+      with: { author: true },
+      extras: { likeCount: countWithColumn(likes.postId.name).as('likeCount') },
+      limit: 30,
+      offset,
+      orderBy: desc(posts.createdAt),
+    })
+
+    return { code: 200, message: 'success', data: queryPosts }
   } catch (error) {
     let message = 'Oops, something went wrong. Please try again later.'
 
@@ -33,21 +56,53 @@ export const usePosts = routeLoader$(async (reqEvent) => {
 
 export default component$(() => {
   const postsSignal = usePosts()
-
   const posts = useStore(postsSignal.value.data ?? [])
 
+  const loadingMore = useSignal(false)
+
   const session = useAuthSession()
+
+  useVisibleTask$(({ cleanup }) => {
+    const nearBottom = async () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+        !loadingMore.value
+      ) {
+        loadingMore.value = true
+
+        const newPosts = await getPosts({ offset: posts.length })
+
+        if (newPosts.code !== 200 || !newPosts.data) return
+
+        if (newPosts?.data?.length === 0) {
+          window.removeEventListener('scroll', nearBottom)
+          return
+        }
+
+        posts.push(...newPosts.data)
+
+        // small timeout to prevent multiple requests
+        setTimeout(() => (loadingMore.value = false), 500)
+      }
+    }
+
+    window.addEventListener('scroll', nearBottom)
+
+    cleanup(() => window.removeEventListener('scroll', nearBottom))
+  })
 
   return (
     <div class="w-[600px] max-w-full flex-grow self-center border-l-[1px] border-r-[1px]">
       <Header />
-      <section class="flex flex-col pb-20 pt-[3.3rem]">
+      <section class="flex flex-col pb-32 pt-[3.3rem]">
         {session.value?.user && <PostForm posts={posts} user={session.value.user} />}
         {postsSignal.value.code !== 200 ? (
           <ErrorMessage message={postsSignal.value.message} />
         ) : (
           posts.map((post) => <PostItem key={post.id} post={post} />)
         )}
+
+        <div class="mt-14 h-2 w-2 self-center rounded-full bg-stone-200 dark:bg-slate-600" />
       </section>
     </div>
   )
