f9e185c8c4b69cda088293b06106c1073b54a29c
Bernardo Quina <bernardoquina@gmail.com>
2023-06-27 18:04:05 +0100

    Remove unused repost id columnn

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ dotenv.config({ path: '.env.local' })
 export default {
   out: './src/db/migrations',
   schema: './src/db/schema.ts',
+  driver: 'mysql2',
   breakpoints: true,
   dbCredentials: { connectionString: process.env.DATABASE_URL || '' },
 } satisfies Config
---
+++ src/db/migrations/0009_heavy_excalibur.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `posts` DROP COLUMN `repost_of_post_id`;
